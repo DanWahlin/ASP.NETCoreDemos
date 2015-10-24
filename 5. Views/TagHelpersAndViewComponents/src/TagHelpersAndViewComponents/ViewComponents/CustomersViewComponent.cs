b4abb5b230641853cab85863c0b9540f78000d89
Dan Wahlin <dwahlin@xmlforasp.net>
2015-10-23 17:22:50 -0700

    Added ViewComponent example back into the project.

---
+++ 5. Views/TagHelpersAndViewComponents/src/TagHelpersAndViewComponents/ViewComponents/CustomersViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelpersAndViewComponents.ViewComponents
+{
+    public class CustomersViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int max)
+        {
+            var items = new List<string>();
+            for (int i = 0; i < max; i++)
+            {
+                items.Add("Customer " + i.ToString());
+            }        
+            return View(items);
+        }
+    }
+}
